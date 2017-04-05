3947407fee8e4afa49b7b23ba52a1eafe6b9d070
Alex Fischer <alexander.fischer3@gmail.com>
2017-04-04 23:44:27 -0400

    added help message to main file

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include "sieve.h"
+#include <string.h>
 
 int main(int argc, char **argv)
 {
+  if (argc > 1 && (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0))
+  {
+    printf("Tries to factor a number, either the first command line argument or 310069 if none is given. Optional second and third arguments: if the second and third arguments are a and b respectively, it will try L(N)^a random numbers in searching for relations and a smoothness bound of L(N)^b. Default values are sqrt(2) and 1/sqrt(2), respectively.\n");
+    exit(0);
+  }
   mpz_t N;
   mpz_init(N);
   float numRelationsPower = sqrt(2), smoothnessPower = 1 / sqrt(2);
